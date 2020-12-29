977692feec9987512c75874949edc94a8e8d03b1
zach wick <zach@zachwick.com>
2020-12-29 09:20:28 -0500

    Initial Commit

---
+++ WordChecker/WordCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordCheckerApp.swift
+//  WordChecker
+//
+//  Created by zach wick on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct WordCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
