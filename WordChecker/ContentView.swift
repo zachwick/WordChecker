55d548501f8a28b990713812dca5966895873471
zach wick <zach@zachwick.com>
2020-12-29 21:50:22 -0500

    Initial working version
    Signed-off-by: zach wick <zach@zachwick.com>

--- WordChecker/ContentView.swift
+++ WordChecker/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    @State var textFieldInput: String = ""
+    @State var predictableValues: Array<String> = []
+    @State var predictedValue: Array<String> = []
+
+    func startGame() {
+       if let startWordsURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
+          if let startWords = try? String(contentsOf: startWordsURL) {
+             let allWords = startWords.components(separatedBy: "\n")
+                predictableValues = allWords
+                return
+          }
+        }
+        fatalError("Could not load words.txt from bundle.")
+    }
+
+    func showMatches() {
+        predictedValue = predictableValues.filter { (item: String) -> Bool in
+            let stringMatch = item.lowercased().starts(with: textFieldInput.lowercased())
+            return stringMatch
+        }
+    }
+
+    var body: some View {
+        VStack(alignment: .leading){
+            TextField("Enter your word", text: $textFieldInput)
+                .onChange(of: textFieldInput, perform: { value in
+                    showMatches()
+                })
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            List(self.predictedValue, id: \.self){
+                Text($0)
+            }
+        }.padding().onAppear(perform: startGame)
     }
 }
