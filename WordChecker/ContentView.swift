9fa57d1cf0ec248d0e7cc0363aa78b229d9c494f
zach wick <zach@zachwick.com>
2020-12-30 00:15:48 -0500

    Adds 2 letter word list and better state handling when searching for an empty string
    Signed-off-by: zach wick <zach@zachwick.com>

--- WordChecker/ContentView.swift
+++ WordChecker/ContentView.swift
@@ -12,12 +12,15 @@ struct ContentView: View {
     @State var textFieldInput: String = ""
     @State var predictableValues: Array<String> = []
     @State var predictedValue: Array<String> = []
+    @State var validWord: Bool = false
+    @State var allTwoLetterWords: Array<String> = []
 
     func startGame() {
        if let startWordsURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
           if let startWords = try? String(contentsOf: startWordsURL) {
              let allWords = startWords.components(separatedBy: "\n")
                 predictableValues = allWords
+                allTwoLetterWords = twoLetterWords()
                 return
           }
         }
@@ -31,16 +34,40 @@ struct ContentView: View {
         }
     }
 
+    func twoLetterWords() -> Array<String> {
+        return predictableValues.filter { (item: String) -> Bool in
+            return item.lowercased().count == 2
+        }
+    }
+
     var body: some View {
-        VStack(alignment: .leading){
-            TextField("Enter your word", text: $textFieldInput)
-                .onChange(of: textFieldInput, perform: { value in
-                    showMatches()
-                })
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            List(self.predictedValue, id: \.self){
-                Text($0)
-            }
-        }.padding().onAppear(perform: startGame)
+        NavigationView {
+            VStack(alignment: .leading){
+                TextField("Enter your word", text: $textFieldInput)
+                    .onChange(of: textFieldInput, perform: { value in
+                        if (textFieldInput == "") {
+                            predictedValue = []
+                        } else {
+                            showMatches()
+                        }
+                    })
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .font(.largeTitle)
+                List(self.predictedValue, id: \.self){
+                    Text($0)
+                }
+                .navigationTitle("Check a word")
+                .navigationBarItems(
+                    trailing:
+                        NavigationLink(
+                            destination: List(self.allTwoLetterWords, id: \.self){
+                                Text($0)
+                            },
+                            label: {
+                                Text("2 letter words")
+                            })
+                )
+            }.padding().onAppear(perform: startGame)
+        }
     }
 }
